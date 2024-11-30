0c44ce21a2ffffe6db0247d7cefc1343404a21aa
Daekyu Lee <dagui0@gmail.com>
2024-12-01 01:51:08 +0900

    2024-12-01 파일 정리

--- testrun.sh
+++ testrun.sh
@@ -2,7 +2,7 @@
 
 exec docker run -d \
   --name nginxif \
-  -p 8080:80/tcp \
+  -p 20080:80/tcp \
   -v `pwd`/etc/nginx:/etc/nginx \
   -v `pwd`/test/00_upstream.conf:/etc/nginx/conf.d/00_upstream.conf \
   -v `pwd`/test/html:/var/www/html \
